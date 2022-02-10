54ac1aee4943f009d76091c7cfad93a60b241e82
Oscar Bernal <oscarbernal93@gmail.com>
2022-02-09 20:02:02 -0500

    Implements Template Engine, add a basic view and static files

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const sprightly = require('sprightly')
+
 const app = express()
 const port = 4000
 
+app.engine('spy', sprightly);
+app.set('view engine', 'spy');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	  res.send('Hello World!')
+	  res.render('home.spy', { message: 'Hello World!' });
 })
 
+
 app.listen(port, () => {
-	  console.log(`Example app listening on port ${port}`)
+	  console.log(`App listening on port ${port}`)
 })
